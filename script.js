6dcb45fe6190bf07cbf5e7f3c4a582d698b57101
ideyou <desenvolvimento.ideyou@gmail.com>
2026-05-22 21:11:06 -0300

    fist commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function toggleTheme() {
+  document.body.classList.toggle("dark-mode");
+}
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 60
+    },
+
+    color: {
+      value: "#ffffff"
+    },
+
+    shape: {
+      type: "circle"
+    },
+
+    opacity: {
+      value: 0.3
+    },
+
+    size: {
+      value: 2
+    },
+
+    move: {
+      enable: true,
+      speed: 1
+    },
+
+    line_linked: {
+      enable: false
+    }
+  }
+});
